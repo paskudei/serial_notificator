d8f371d7d07cda725bb80922cda03d29f3d684e7
Denis Blokhin <paskudeidb@gmail.com>
2024-03-07 17:02:10 +0300

    Добавление парсера для AnimeGo (#2)

---
+++ app/services/anime_go/anime/fetch_episode_release_schedule_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module AnimeGo
+  module Anime
+    class FetchEpisodeReleaseScheduleService
+      attr_reader :html
+
+      def initialize(html:)
+        @html = html
+      end
+
+      def call
+        episode_release_schedule_div.map do |episode_div|
+          episode_div_info = episode_div.children[0].children[0].children
+          result = episode_div_info.first(3).map(&:text)
+          result << episode_div_info.last.children[0].present? ? true : false
+          result
+        end
+      end
+
+      private
+
+      def html_document
+        Nokogiri::HTML(html)
+      end
+
+      def episode_release_schedule_div
+        html_document.xpath("//div[@class='row released-episodes-container']")[0].children.first(3)
+      end
+    end
+  end
+end
