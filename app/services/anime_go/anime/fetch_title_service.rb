bb8a4681bd7aeafad6a3f93f2b611530858eb20d
Denis Blokhin <paskudeidb@gmail.com>
2024-03-11 02:08:56 +0300

    Добавлена логика роллбара

--- app/services/anime_go/anime/fetch_title_service.rb
+++ app/services/anime_go/anime/fetch_title_service.rb
@@ -10,11 +10,25 @@ module AnimeGo
       end
 
       def call
+        raise Error, html_document unless fetch_title
+
+        fetch_title
+      end
+
+      private
+
+      def html_document
         Nokogiri::HTML(html)
-                &.xpath("//div[@class='anime-title']")&.[](0)
-                &.children&.[](0)
-                &.children&.[](0)
-                &.text
+      end
+
+      def fetch_title
+        html_document&.xpath("//div[@class='anime-title']")&.[](0)
+                       &.children&.[](0)
+                       &.children&.[](0)
+                       &.text
+      end
+
+      class Error < StandardError
       end
     end
   end
